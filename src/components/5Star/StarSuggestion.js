3e580a29f468bbcb9aac594126904931a0198459
rslgp <rslgp@cin.ufpe.br>
2023-08-06 14:56:32 +0000

    update

--- src/components/5Star/StarSuggestion.js
+++ src/components/5Star/StarSuggestion.js
@@ -3,14 +3,16 @@ import './StarSuggestion.css';
 
 const StarSuggestion = (props) => {
   const [rating, setRating] = useState(props.rating ? props.rating : 0);
-  const [lastRatingTime, setLastRatingTime] = useState(null);
-
+  //const [lastRatingTime, setLastRatingTime] = useState(null);
+  const lastRatingTime = sessionStorage.getItem('lastRatingTime');
+  
   const handleStarClick = (value) => {
     const limitMin = 2;
     const currentTime = Date.now();
     if (!lastRatingTime || currentTime - lastRatingTime >= limitMin * 60 * 1000) {
       setRating(value);
-      setLastRatingTime(currentTime);
+      //setLastRatingTime(currentTime);
+      sessionStorage.setItem('lastRatingTime', currentTime);
       // Here you can perform any action like submitting the rating to a server or saving it in a database
     } else {
       // The user can rate only once per 5 minutes
